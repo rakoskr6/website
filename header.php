6064086f8ba68494b5ae5b1871cc1fbbe1362ea4
Kyle Rakos <rakoskr6@gmail.com>
2017-11-20 22:39:56 -0500

    Reformatted header loading and reordered folder structure

--- header.php
+++ header.php
@@ -9,42 +9,46 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?php echo $title;?></title>
+    <title>
+        <?php 
+            // Only displays title if set
+            if ($title == '') {
+                echo 'Kyle Rakos';
+            }
+            else {
+                echo $title;
+            }
+        ?>
+    </title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Components -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- Custom CSS -->
-    <link href="/css/landing-page.css" rel="stylesheet">
+    <link href="assets/css/landing-page.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="/text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="/text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="/text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="/text/css">
 
 
 	<style>
-	legend { 
-    display: block;
-    padding-left: 2px;
-    padding-right: 2px;
-	padding-top: 48px;
-    border: none;
-	}
-	body { 
-    padding-top: 65px; 
-	}
-	.disabledTab{
-    pointer-events: none;
-	}
+    	legend { 
+            display: block;
+            padding-left: 2px;
+            padding-right: 2px;
+        	padding-top: 48px;
+            border: none;
+    	}
+    	body { 
+            padding-top: 65px; 
+    	}
+    	.disabledTab{
+            pointer-events: none;
+    	}
 	</style>
 </head>
+
 
 <body>
     <!-- Navigation -->
